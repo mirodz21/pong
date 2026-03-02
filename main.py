436294ab29aa5e2046ee8d476b4e94a4e5430a88
IanRodriguez <mirodriguez.dev@gmail.com>
2026-03-02 20:33:54 +0800

    scoring function added. gradual increase of ball movement added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from turtle import Screen
+
+import scoreboard
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 MAX_Y = 230
@@ -13,6 +16,10 @@ screen.tracer(0)
 
 l_paddle = Paddle((-390, 0))
 r_paddle = Paddle((380, 0))
+
+l_score = Scoreboard((-100,190))
+r_score = Scoreboard((100,190))
+
 ball = Ball()
 
 screen.listen()
@@ -24,7 +31,7 @@ screen.onkey(r_paddle.move_down, "Down")
 
 game_over = False
 while not game_over:
-    time.sleep(.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -32,8 +39,17 @@ while not game_over:
     if ball.ycor() > MAX_Y or ball.ycor() < -MAX_Y:
         ball.bounce_y()
 
+    # BOUNCE IF COLLISION WITH PADDLE
     if (ball.distance(r_paddle) < 30 and ball.xcor() > 330 or
     ball.distance(l_paddle) < 30 and ball.xcor() < -330):
         ball.bounce_x()
+
+    # SCORE AND RESET IF BALL GOES OUT OF BOUND
+    if ball.xcor() > 380:
+        l_score.add_score()
+        ball.ball_reset()
+    if ball.xcor() < -380:
+        ball.ball_reset()
+        r_score.add_score()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -3,8 +3,26 @@ from turtle import Turtle
 
 class Scoreboard(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.color("white")
         self.penup()
         self.hideturtle()
+        self.goto(position)
+        self.score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"{self.score}", False, "center", ("Courier", 30, "bold"))
+
+    def add_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
+
+    def display_game_over(self):
+        self.goto(0,0)
+        self.color("RED")
+        self.write("Game Over",False, "center", ("Courier",8,"normal"))
+
+
